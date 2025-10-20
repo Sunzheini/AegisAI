23acbd3a821296c2a0f5a1d5d2c1a59ab8023880
Sunzheini <daniel_zorov@abv.bg>
2025-10-20 15:12:28 +0300

    Remove outdated TODO comment regarding validation worker implementation

--- services/api-gateway-service/workflow_orchestrator_example.py
+++ services/api-gateway-service/workflow_orchestrator_example.py
@@ -74,7 +74,6 @@ later: add user stories use jira, pull requests how to see progress in github
 """
 
 
-# ToDo: Finish validation worker real validation logic and tests
 # ToDo: Other workers 1 by 1 similar to the validation worker with client and service
 # ToDo: when moving Give the workflow orchestrator direct access to the storage via shared
 #  folder, it is better to pass only the file path and metadata in the job request, not the
